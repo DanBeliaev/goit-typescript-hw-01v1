9b12b85da0b710aeb329e1fb42a804970088f9d9
DanBeliaev <beliaev1108@gmail.com>
2024-10-11 00:45:41 +0100

    ff

---
+++ src/basic/1.ts
@@ -0,0 +1,11 @@
+const age = 50;
+const username = 'Max';
+const toggle = true;
+const empty = null;
+const callback = (a: number): number => { return 100 + a };
+
+age;
+username;
+toggle;
+empty;
+callback;
---
+++ src/basic/3.ts
@@ -0,0 +1,6 @@
+let comeValue: (string | number) = 55;
+type OneOrTwo = 'enable' | 'disable';
+const value: OneOrTwo = 'enable';
+
+comeValue;
+value;
